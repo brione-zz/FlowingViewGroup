a75fa0c3d3cd65a28619b0057f5abe2b76d76d44
unknown <Brion Emde@.(none)>
2010-01-08 17:21:59 -0700

    fixed most of the problems that were there

--- src/com/eyebrowssoftware/example/fvg/TagsViewGroup.java
+++ src/com/eyebrowssoftware/example/fvg/TagsViewGroup.java
@@ -7,6 +7,7 @@ import junit.framework.Assert;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
@@ -84,17 +85,6 @@ public class TagsViewGroup extends ViewGroup {
 		}
 		
 		a.recycle();
-		int i = this.getPaddingTop();
-		i = this.getPaddingBottom();
-		i = this.getPaddingLeft();
-		i = this.getPaddingRight();
-
-		i = params.bottomMargin;
-		i = params.topMargin;
-		i = params.leftMargin;
-		i = params.rightMargin;
-
-	
 	}
 	
 	public void setHorizontalSpacing(int hSpacing) {
@@ -140,16 +130,24 @@ public class TagsViewGroup extends ViewGroup {
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+		
+		Log.d(TAG, "-----------------------------------------");
+		Log.d(TAG, "Measure Dimensions: widthSize = " + String.valueOf(widthSize) 
+				+ " heightSize: " + String.valueOf(heightSize));
+
+		
 		mPaddingTop = getPaddingTop();
 		mPaddingBottom = getPaddingBottom();
 		mPaddingLeft = getPaddingLeft();
 		mPaddingRight = getPaddingRight();
 		
-		//super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+		Log.d(TAG, "Measure Padding: Top = " + String.valueOf(mPaddingTop) + "Bottom: " + String.valueOf(mPaddingBottom) 
+				+ " Left = " + String.valueOf(mPaddingLeft) + " Right = " + String.valueOf(mPaddingRight));
 		
 		int measuredHeight = 0;
 		int measuredWidth= 0;
@@ -158,36 +156,50 @@ public class TagsViewGroup extends ViewGroup {
 		
 		int suggestedMinWidth = this.getSuggestedMinimumWidth();
 		int suggestedMinHeight = this.getSuggestedMinimumHeight();
+
+		int height = 0;
+		int width = 0;
 		
 		if(widthMode == MeasureSpec.UNSPECIFIED) {
+			Log.d(TAG, "Unspecified");
 			// we'll have a single row of tags, respecing the padding.
 			int ccount = this.getChildCount();
 			for(int i = 0; i < ccount; ++i) {
 				TextView tv = (TextView) this.getChildAt(i);
 				tv.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+				height = tv.getMeasuredHeight();
+				width = tv.getMeasuredWidth();
+				Log.d(TAG, "Unspecified Simple: width = " + String.valueOf(width) + " height: " + String.valueOf(height));
 				children_width += tv.getMeasuredWidth();
 				if((i+1) < ccount)
 					children_width += mHorizontalSpacing;
 			}
+			Log.d(TAG, "-----------------------------------------");
 			measuredHeight = mPaddingTop + mTextHeight + mPaddingBottom;
 			measuredWidth = children_width + mPaddingLeft + mPaddingRight;
 		} else {	// MeasureSpec.AT_MOST or MeasureSpec.EXACTLY 
+			Log.d(TAG, "Other");
 			measuredWidth = widthSize; // use the max allowed
-			int right = widthSize - mPaddingRight -1; 
-			int next_pos = mPaddingLeft;
+			children_width = widthSize - mPaddingLeft - mPaddingRight;
+			int pos = 0;
 			int row = 0;
-			int width = 0;
 			int ccount = this.getChildCount();
 			for(int i = 0; i < ccount; ++i) {
 				TextView tv = (TextView) this.getChildAt(i);
 				tv.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+				height = tv.getMeasuredHeight();
 				width = tv.getMeasuredWidth();
-				if(next_pos + width >= right) {
-					next_pos = mPaddingLeft;
+				Log.d(TAG, "Other Simple: width = " + String.valueOf(width) + " height: " + String.valueOf(height));
+				if(pos + width + mHorizontalSpacing >= children_width) {
+					Log.d(TAG, "Starting new row, next_post = " + String.valueOf(pos));
+					pos = 0;
 					++row; 
 				}
-				next_pos += width + mHorizontalSpacing;
+				pos += width + mHorizontalSpacing;
+				Log.d(TAG, "Extending row, next_post = " + String.valueOf(pos));
 			}
+			Log.d(TAG, "-----------------------------------------");
+			
 			int rows_written = ++row;
 			switch(heightMode){
 			case MeasureSpec.AT_MOST:
@@ -204,7 +216,7 @@ public class TagsViewGroup extends ViewGroup {
 				// Log.d(TAG, "heightMode: UNSPECIFIED");
 				if(rows_written < mMinRows)
 					rows_written = mMinRows;
-				children_height = row * (mTextHeight + mVerticalSpacing) - mVerticalSpacing;
+				children_height = rows_written * (mTextHeight + mVerticalSpacing) - mVerticalSpacing;
 				measuredHeight = children_height + mPaddingTop + mPaddingBottom;
 				break;
 			default:
@@ -216,22 +228,33 @@ public class TagsViewGroup extends ViewGroup {
 		if(measuredWidth < suggestedMinWidth)
 			measuredWidth = suggestedMinWidth;
 		setMeasuredDimension(measuredWidth, measuredHeight);
+		Log.d(TAG, "Measurement: width = " + String.valueOf(measuredWidth) + " height: " + String.valueOf(measuredHeight));
 	}
 	
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+		Log.d(TAG, "Layout Dimensions: Top = " + String.valueOf(t) + "Bottom: " + String.valueOf(b) 
+				+ " Left = " + String.valueOf(l) + " Right = " + String.valueOf(r));
+
+		Log.d(TAG, "Layout Padding: Top = " + String.valueOf(mPaddingTop) + "Bottom: " + String.valueOf(mPaddingBottom) 
+				+ " Left = " + String.valueOf(mPaddingLeft) + " Right = " + String.valueOf(mPaddingRight));
+		
 		int pos = mPaddingLeft;
 		int row = 0;
 		r -= mPaddingRight;
 		for(int i = 0; i < this.getChildCount(); ++i) {
 			TextView tv = (TextView) this.getChildAt(i);
 			int width = tv.getMeasuredWidth();
-			if((pos + width) >= r) {
+			int height = tv.getMeasuredHeight();
+			if((pos + width + mHorizontalSpacing) >= r) {
 				pos = mPaddingLeft;
 				++row; 
 			}
 			int child_top = mPaddingTop + row * (mTextHeight + mVerticalSpacing);
-			tv.layout(pos, child_top, pos + width, child_top + mTextHeight);
+			tv.layout(pos, child_top, pos + width, child_top + height);
+			Log.d(TAG, "child: Top = " + String.valueOf(child_top) + "Bottom: " + String.valueOf(child_top+height) 
+					+ " Left = " + String.valueOf(pos) + " Right = " + String.valueOf(pos + width));
 			pos += width + mHorizontalSpacing;
 		}
 	}
